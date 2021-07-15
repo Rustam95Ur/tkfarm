bab81d2793d40adeb285721c8d7e1c2cd2e7d820
kurbanov.rustam <kurbanov.rustam@applecity.kz>
2021-07-15 23:23:56 +0600

    fix code

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -121,7 +121,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/var/www/tkfarm/backend/static/'
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
